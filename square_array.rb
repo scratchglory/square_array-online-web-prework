c38548786310b99601ac952d261ad534b13a68eb
Adella <adellakenn@gmail.com>
2019-04-28 18:15:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+# return value of calling .each on an array
+# operate on each element of an array and collect/store those elements
+# Square a number
+# each { |item| block } 
+
+
+
 def square_array(array)
-  # your code here
+  arr = []
+  array.each { |i| arr << i **2 } #with each element, takes the value, "shovels"/
+  arr
 end
